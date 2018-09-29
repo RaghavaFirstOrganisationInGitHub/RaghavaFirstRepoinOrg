8be217c7dc6dfc266b8f34b05270de3451026c89
svchraghava <37681830+svchraghava@users.noreply.github.com>
2018-09-30 00:24:24 +0800

    CS file in Dev branch

---
+++ TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication4.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        string GetString()
+        {
+            return "Hello World is old now. It’s time for wassup bro ;)";
+        }
+    }
+}
